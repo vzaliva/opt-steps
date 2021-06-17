2de43223d39ccd7beed3a59fcd4713304adcd2ce
Vadim Zaliva <lord@crocodile.org>
2021-06-17 10:22:11 -0700

    initial version

---
+++ opt-steps.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+import subprocess
+import click
+
+def get_passes(llname):
+    p = subprocess.check_output(['opt', '-O3', '-opt-bisect-limit=-1', '-S', llname, '-o', '/dev/null'], stderr=subprocess.STDOUT, text=True)
+    pl = list(map(str.strip, p.split('\n')))
+    res = []
+    for s in pl:
+        m = re.match(r"""BISECT: running pass \((\d+)\) (.*)""", s)
+        if not m is None:
+            res.append((int(m.group(1)), m.group(2)))
+    res.sort(key = lambda x: x[0])
+    return(res)
+
+@click.command()
+@click.version_option("1.0")
+@click.option('--verbose', '-v', is_flag=True, help='Enables verbose mode.')
+@click.argument('cname')
+def main(verbose, cname):
+    if not cname.endswith(".c"):
+        print("Argument %s must have .c extension" % cname)
+        sys.exit(1)
+    if not os.path.exists(cname):
+        print("File %s does not exist" % cname)
+        sys.exit(1)
+    
+    name = cname[:-2]
+    llname = name + '.ll'
+    # Generate LL file
+    subprocess.check_call(['clang', '-emit-llvm', '-S', '-O0', cname, '-o', llname])
+
+    passes = get_passes(llname)
+    prev = llname
+    for (pn,ps) in passes:
+        pname = "%s.%d.ll" % (name,pn)
+        print("Generating %s: %s" % (pname,ps))
+        subprocess.check_call(['opt', '-O3', ("-opt-bisect-limit="+str(pn)), '-S', llname, '-o', pname], stderr=subprocess.DEVNULL)
+        result = subprocess.run(['diff', '-q', prev, pname], stdout=subprocess.DEVNULL)
+        if result.returncode == 0:
+            print("\t unchanged")
+            os.remove(pname)
+        else:
+            print("\t CHANGED")
+            prev = pname
+
+    
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
